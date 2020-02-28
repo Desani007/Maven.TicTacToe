ac46887b3a9e7c3f0b9aa73f65f73a7a4c60428d
dbond <debon1426@gmail.com>
2020-02-28 18:14:25 -0500

    finished

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,148 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix=matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        if( getWinner().equals("O")|| (isTie())){
+            return false;
+        } else if (getWinner().equals("X")|| (isTie())){
+            return true;
+
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                    if ((matrix[0][0] == 'X') && (matrix[0][1] == 'X')) {
+                        return true;
+                    } else if ((matrix[0][1] == 'X') && (matrix[0][2] == 'X')) {
+                        return true;
+                    } else if ((matrix[1][0] == 'X') && (matrix[1][1] == 'X')) {
+                        return true;
+                    } else if ((matrix[1][1] == 'X') && (matrix[1][2] == 'X')) {
+                        return true;
+                    } else if ((matrix[0][0] == 'X') && (matrix[1][0] == 'X')) {
+                        return true;
+                    } else if ((matrix[0][1] == 'X') && (matrix[1][1] == 'X')) {
+                        return true;
+                    } else if ((matrix[0][2] == 'X') && (matrix[1][2] == 'X')) {
+                        return true;
+                    } else if ((matrix[2][0] == 'X') && (matrix[2][1] == 'X')) {
+                        return true;
+                    } else if ((matrix[2][1] == 'X') && (matrix[2][2] == 'X')) {
+                        return true;
+                    } else if ((matrix[1][0] == 'X') && (matrix[2][0] == 'X')) {
+                        return true;
+                    } else if ((matrix[1][1] == 'X') && (matrix[2][1] == 'X')) {
+                        return true;
+                    } else if ((matrix[1][2] == 'X') && (matrix[2][2] == 'X')) {
+                        return true;
+
+                }
+            }
+
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if( getWinner().equals("X")||(isTie())){
+            return false;
+        } else if (getWinner().equals("O")|| (isTie())){
+            return true;
+
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                if ((matrix[0][0] == 'O') && (matrix[0][1] == 'O')) {
+                    return true;
+                } else if ((matrix[0][1] == 'O') && (matrix[0][2] == 'O')) {
+                    return true;
+                } else if ((matrix[1][0] == 'O') && (matrix[1][1] == 'O')) {
+                    return true;
+                } else if ((matrix[1][1] == 'O') && (matrix[1][2] == 'O')) {
+                    return true;
+                } else if ((matrix[0][0] == 'O') && (matrix[1][0] == 'O')) {
+                    return true;
+                } else if ((matrix[0][1] == 'O') && (matrix[1][1] == 'O')) {
+                    return true;
+                } else if ((matrix[0][2] == 'O') && (matrix[1][2] == 'O')) {
+                    return true;
+                } else if ((matrix[2][0] == 'O') && (matrix[2][1] == 'O')) {
+                    return true;
+                } else if ((matrix[2][1] == 'O') && (matrix[2][2] == 'O')) {
+                    return true;
+                } else if ((matrix[1][0] == 'O') && (matrix[2][0] == 'O')) {
+                    return true;
+                } else if ((matrix[1][1] == 'O') && (matrix[2][1] == 'O')) {
+                    return true;
+                } else if ((matrix[1][2] == 'O') && (matrix[2][2] == 'O')) {
+                    return true;
+
+                }
+            }
+
+        }
+        return false;
     }
 
+
     public Boolean isTie() {
-        return null;
+        if (getWinner()==("")){
+            return true;
+        } else
+        return false;
     }
 
     public String getWinner() {
-        return null;
+
+        for (int i=0; i<matrix.length;i++){
+            for (int j=0; j<matrix[i].length;j++) {
+                if ((matrix[0][0] == 'X') && (matrix[0][1] == 'X') && (matrix[0][2] == 'X')) {
+                    return String.valueOf('X');
+                } else if ((matrix[0][0] == 'O') && (matrix[0][1] == 'O') && (matrix[0][1] == 'O')) {
+                    return String.valueOf('O');
+                } else if ((matrix[0][0] == 'O') && (matrix[1][1] == 'O') && (matrix[2][2] == 'O')) {
+                    return String.valueOf('O');
+                } else if ((matrix[0][0] == 'X') && (matrix[1][1] == 'X') && (matrix[2][2] == 'X')) {
+                    return String.valueOf('X');
+                }else if ((matrix[0][0] == 'X') && (matrix[1][0] == 'X') && (matrix[2][0] == 'X')) {
+                    return String.valueOf('X');
+                }else if ((matrix[0][0] == 'O') && (matrix[1][0] == 'O') && (matrix[2][0] == 'O')) {
+                    return String.valueOf('O');
+                }else if ((matrix[0][1] == 'O') && (matrix[1][1] == 'O') && (matrix[2][1] == 'O')) {
+                    return String.valueOf('O');
+                }else if ((matrix[0][1] == 'X') && (matrix[1][1] == 'X') && (matrix[2][1] == 'X')) {
+                    return String.valueOf('X');
+                }else if ((matrix[0][2] == 'O') && (matrix[1][2] == 'O') && (matrix[2][2] == 'O')) {
+                    return String.valueOf('O');
+                }else if ((matrix[0][2] == 'X') && (matrix[1][2] == 'X') && (matrix[2][2] == 'X')) {
+                    return String.valueOf('X');
+                }else if ((matrix[1][0] == 'O') && (matrix[1][1] == 'O') && (matrix[1][2] == 'O')) {
+                    return String.valueOf('O');
+                }else if ((matrix[1][0] == 'X') && (matrix[1][1] == 'X') && (matrix[1][2] == 'X')) {
+                    return String.valueOf('X');
+                }else if ((matrix[2][0] == 'X') && (matrix[2][1] == 'X') && (matrix[2][2] == 'X')) {
+                    return String.valueOf('X');
+                }else if ((matrix[2][0] == 'O') && (matrix[2][1] == 'O') && (matrix[2][2] == 'O')) {
+                    return String.valueOf('O');
+                }else if ((matrix[0][2] == 'O') && (matrix[1][1] == 'O') && (matrix[2][0] == 'O')) {
+                    return String.valueOf('O');
+                }else if ((matrix[0][2] == 'X') && (matrix[1][1] == 'X') && (matrix[2][0] == 'X')) {
+                    return String.valueOf('X');
+                }
+
+
+
+
+            }
+        }
+        return "";
     }
 
 }
